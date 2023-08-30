71daf88657b5e2d0ac355bec829784457a117c4d
usha101111 <sapram.usha10@gmail.com>
2023-08-30 16:42:41 +0530

    added all

---
+++ src/main/java/in/usha/App.java
@@ -0,0 +1,13 @@
+package in.usha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
